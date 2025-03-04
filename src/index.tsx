1f2a7c8f0ba15490c91740291e24651d57487421
Parikshah <Parishah@udel.edu>
2025-03-04 18:28:48 -0500

    Merging issues resolved

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
